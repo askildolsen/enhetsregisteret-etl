7316475ed8ed4c6bbe371f948b16d387b4afa6e9
Askild Olsen <askild@xangeli.com>
2018-01-02 22:58:42 +0100

    Les gzipped request

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
 
 namespace enhetsregisteret_etl
 {
@@ -6,7 +9,21 @@ namespace enhetsregisteret_etl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WebRequest request = WebRequest.Create("http://data.brreg.no/enhetsregisteret/download/enheter");
+            using (WebResponse response = request.GetResponse())
+            {
+                using (var stream = response.GetResponseStream())
+                using (var gzipstream = new GZipStream(stream, CompressionMode.Decompress))
+                using (StreamReader reader = new StreamReader(gzipstream))
+                {
+                    while(!reader.EndOfStream)
+                    {
+                        var ret = reader.ReadLine();
+                        Console.WriteLine(ret);
+                    }
+                }
+            }            
         }
     }
 }
+
