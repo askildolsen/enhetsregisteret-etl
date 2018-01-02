2eba03f90bafbefc651f7c228c4eb5f3b4e1f0be
Askild Olsen <askild@xangeli.com>
2018-01-02 21:40:48 +0100

    dot net new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace enhetsregisteret_etl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
